80f903cf27c465f87843e98fcc0711b62ccea6ab
Demir Kasyan <korvo0atano@gmail.com>
2020-04-19 17:48:27 +0300

    v0.5.9

--- three.scripts/MainControl.js
+++ three.scripts/MainControl.js
@@ -5,7 +5,7 @@ import {
 	Vector3
 } from "../three.addons/three.module.js";
 
- import GyroNorm from "../three.addons/gyronorm.js";
+ import * as GyroNorm from "../three.addons/gyronorm.js";
 
 var MainControl = function ( object ) {
 
